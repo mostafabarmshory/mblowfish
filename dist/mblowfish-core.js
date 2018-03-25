cbe027a1284000c89ad51f02caefa7666a8378eb
mostafabarmshory <mostafa.barmshory@dpq.co.ir>
2018-03-25 16:46:11 +0430

    Adding eclipse builder.
    Adding tern configs.

--- dist/mblowfish-core.js
+++ dist/mblowfish-core.js
@@ -306,296 +306,179 @@ angular.module('mblowfish-core')
  */
 .controller('MbAccountCtrl', function($scope, $app, $navigator) {
 
-	/*
-	 * Store controller state
-	 */
-	var ctrl = {
-			changingPassword: false
-	};
+    /*
+     * Store controller state
+     */
+    var ctrl = {
+            changingPassword: false
+    };
+    
+    /**
+     * Go to the default page
+     * 
+     * @name load
+     * @memberof AmdAccountCtrl
+     * @returns {promiss} to load user data
+     */
+    function goToDashboard() {
+        // XXX: maso, 1395: ممکن هست این حالت وجود نداشته باشد
+        $navigator.openPage('dashboard');
+    }
+
+    /**
+     * Call login process for current user
+     * 
+     * @memberof AmdAccountCtrl
+     * @name login
+     * @param {object}
+     *            cridet username and password
+     * @param {string}
+     *            cridet.login username
+     * @param {stirng}
+     *            cridig.password Password
+     * @returns {promiss} to do the login
+     */
+    function login(cridet) {
+        if(ctrl.loadUser){
+            return;
+        }
+        ctrl.loadUser= true;
+        return $app.login(cridet)//
+        .catch(function(error) {
+            ctrl.error = error;
+        })//
+        .finally(function(){
+            ctrl.loadUser = false;
+        });
+    }
 
 
-	/**
-	 * Go to the default page
-	 * 
-	 * @name load
-	 * @memberof AmdAccountCtrl
-	 * @returns {promiss} to load user data
-	 */
-	function goToDashboard() {
-		// XXX: maso, 1395: ممکن هست این حالت وجود نداشته باشد
-		$navigator.openPage('dashboard');
-	}
-
-	/**
-	 * Call login process for current user
-	 * 
-	 * @memberof AmdAccountCtrl
-	 * @name login
-	 * @param {object} cridet          username and password
-	 * @param {string} cridet.login    username
-	 * @param {stirng} cridig.password Password
-	 * @returns {promiss} to do the login
-	 */
-	function login(cridet) {
-		if(ctrl.loadUser){
-			return;
-		}
-		ctrl.loadUser= true;
-		return $app.login(cridet)//
-		.catch(function(error) {
-			ctrl.error = error;
-		})//
-		.finally(function(){
-			ctrl.loadUser = false;
-		});
-	}
+    /**
+     * Loads user data
+     * 
+     * @name load
+     * @memberof AmdAccountCtrl
+     * @returns {promiss} to load user data
+     */
+    function loadUser(){
+        if(ctrl.loadUser){
+            return;
+        }
+        ctrl.loadUser = true;
+        return $usr.session()//
+        .then(function(user){
+            setUser(user);
+            ctrl.loadUser = false;
+        }, function(error){
+            ctrl.error = error;
+        })//
+        .finally(function(){
+            ctrl .loadUser = false;
+        });
+    }
 
 
-	/**
-	 * Loads user data
-	 * 
-	 * @name load
-	 * @memberof AmdAccountCtrl
-	 * @returns {promiss} to load user data
-	 */
-	function loadUser(){
-		if(ctrl.loadUser){
-			return;
-		}
-		ctrl.loadUser = true;
-		return $usr.session()//
-		.then(function(user){
-			setUser(user);
-			ctrl.loadUser = false;
-		}, function(error){
-			ctrl.error = error;
-		})//
-		.finally(function(){
-			ctrl .loadUser = false;
-		});
-	}
-	
+    /**
+     * Save current user
+     * 
+     * @name load
+     * @memberof AmdAccountCtrl
+     * @returns {promiss} to save
+     */
+    function saveUser(){
+        if(ctrl.loadUser){
+            return;
+        }
+        // TODO: maso, 2017: check if user exist
+        ctrl.saveUser = true;
+        return ctrl.user.update()//
+        .then(function(){
+            ctrl.saveUser = false;
+        }, function(error){
+            ctrl.error = error;
+            ctrl.saveUser = false;
+        })
+    }
 
-	/**
-	 * Save current user
-	 * 
-	 * @name load
-	 * @memberof AmdAccountCtrl
-	 * @returns {promiss} to save
-	 */
-	function saveUser(){
-		if(ctrl.loadUser){
-			return;
-		}
-		// TODO: maso, 2017: check if user exist
-		ctrl.saveUser = true;
-		return ctrl.user.update()//
-		.then(function(){
-			ctrl.saveUser = false;
-		}, function(error){
-			ctrl.error = error;
-			ctrl.saveUser = false;
-		})
-	}
-
-	/**
-	 * Change password of the current user
-	 * 
-	 * @name load
-	 * @memberof AmdAccountCtrl
-	 * @returns {promiss} to change password
-	 */
-	function changePassword(data) {
-		if(ctrl.changingPassword){
-			return;
-		}
-		ctrl.changingPassword = true;
-		var param = {
-				'old' : data.oldPass,
-				'new' : data.newPass,
-				'password': data.newPass,
-		};
-//		return $usr.resetPassword(param)//
-		$scope.app.user.current.newPassword(param)
-		.then(function(){
-			$scope.logout();
-		}, function(error){
-			alert('Fail to update password:'+error.data.message);
-		})//
-		.finally(function(){
-			ctrl.changingPassword = false;
-		});
-	}
-	
-
-	/**
-	 * Update avatar of the current user
-	 * 
-	 * @name load
-	 * @memberof AmdAccountCtrl
-	 * @returns {promiss} to update avatar
-	 */
-	function updateAvatar(avatarFiles){
-		// XXX: maso, 1395: reset avatar
-		return ctrl.user.newAvatar(avatarFiles[0].lfFile)//
-		.then(function(){
-			$window.location.reload();
-		}, function(error){
-			alert('Fail to update avatar:' + error.data.message);
-		});
-	}
+    /**
+     * Change password of the current user
+     * 
+     * @name load
+     * @memberof AmdAccountCtrl
+     * @returns {promiss} to change password
+     */
+    function changePassword(data) {
+        if(ctrl.changingPassword){
+            return;
+        }
+        ctrl.changingPassword = true;
+        var param = {
+                'old' : data.oldPass,
+                'new' : data.newPass,
+                'password': data.newPass,
+        };
+//      return $usr.resetPassword(param)//
+        $scope.app.user.current.newPassword(param)
+        .then(function(){
+            $scope.logout();
+        }, function(error){
+            alert('Fail to update password:'+error.data.message);
+        })//
+        .finally(function(){
+            ctrl.changingPassword = false;
+        });
+    }
 
 
-	// TODO: maso, 2017: getting from server
-	// Account property descriptor
-	$scope.apds = [ {
-		key : 'first_name',
-		title : 'First name',
-		type : 'string'
-	}, {
-		key : 'last_name',
-		title : 'Last name',
-		type : 'string'
-	}, {
-		key : 'language',
-		title : 'language',
-		type : 'string'
-	}, {
-		key : 'timezone',
-		title : 'timezone',
-		type : 'string'
-	} ];
-	
-	// Bind to scope
-	$scope.ctrl = ctrl;
-	$scope.login = login;
-	$scope.logout = $app.logout;
-	$scope.goToDashboard = goToDashboard;
-	$scope.load = loadUser;
-	$scope.reload = loadUser;
-	$scope.saveUser = saveUser;
-	$scope.changePassword = changePassword;
-	$scope.updateAvatar = updateAvatar;
+    /**
+     * Update avatar of the current user
+     * 
+     * @name load
+     * @memberof AmdAccountCtrl
+     * @returns {promiss} to update avatar
+     */
+    function updateAvatar(avatarFiles){
+        // XXX: maso, 1395: reset avatar
+        return ctrl.user.newAvatar(avatarFiles[0].lfFile)//
+        .then(function(){
+            $window.location.reload();
+        }, function(error){
+            alert('Fail to update avatar:' + error.data.message);
+        });
+    }
+
+
+    // TODO: maso, 2017: getting from server
+    // Account property descriptor
+    $scope.apds = [ {
+        key : 'first_name',
+        title : 'First name',
+        type : 'string'
+    }, {
+        key : 'last_name',
+        title : 'Last name',
+        type : 'string'
+    }, {
+        key : 'language',
+        title : 'language',
+        type : 'string'
+    }, {
+        key : 'timezone',
+        title : 'timezone',
+        type : 'string'
+    } ];
+
+    // Bind to scope
+    $scope.ctrl = ctrl;
+    $scope.login = login;
+    $scope.logout = $app.logout;
+    $scope.goToDashboard = goToDashboard;
+    $scope.load = loadUser;
+    $scope.reload = loadUser;
+    $scope.saveUser = saveUser;
+    $scope.changePassword = changePassword;
+    $scope.updateAvatar = updateAvatar;
 });
-
-
-// TODO: Hadi: should be deleted. MbPreferencesCtrl is replaced.
-///*
-// * Copyright (c) 2015-2025 Phoinex Scholars Co. http://dpq.co.ir
-// * 
-// * Permission is hereby granted, free of charge, to any person obtaining a copy
-// * of this software and associated documentation files (the "Software"), to deal
-// * in the Software without restriction, including without limitation the rights
-// * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// * copies of the Software, and to permit persons to whom the Software is
-// * furnished to do so, subject to the following conditions:
-// * 
-// * The above copyright notice and this permission notice shall be included in all
-// * copies or substantial portions of the Software.
-// * 
-// * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-// * SOFTWARE.
-// */
-//'use strict';
-//angular.module('mblowfish-core')
-//
-//
-///**
-// * @ngdoc controller
-// * @name AmdConfigsCtrl
-// * @description Controller of configs
-// * 
-// */
-//.controller('AmdConfigsCtrl', function($scope, $settings) {
-//
-//	var COLORS = [ '#ffebee', '#ffcdd2', '#ef9a9a', '#e57373',
-//		'#ef5350', '#f44336', '#e53935', '#d32f2f', '#c62828',
-//		'#b71c1c', '#ff8a80', '#ff5252', '#ff1744', '#d50000',
-//		'#f8bbd0', '#f48fb1', '#f06292', '#ec407a', '#e91e63',
-//		'#d81b60', '#c2185b', '#ad1457', '#880e4f', '#ff80ab',
-//		'#ff4081', '#f50057', '#c51162', '#e1bee7', '#ce93d8',
-//		'#ba68c8', '#ab47bc', '#9c27b0', '#8e24aa', '#7b1fa2',
-//		'#4a148c', '#ea80fc', '#e040fb', '#d500f9', '#aa00ff',
-//		'#ede7f6', '#d1c4e9', '#b39ddb', '#9575cd', '#7e57c2',
-//		'#673ab7', '#5e35b1', '#4527a0', '#311b92', '#b388ff',
-//		'#7c4dff', '#651fff', '#6200ea', '#c5cae9', '#9fa8da',
-//		'#7986cb', '#5c6bc0', '#3f51b5', '#3949ab', '#303f9f',
-//		'#283593', '#1a237e', '#8c9eff', '#536dfe', '#3d5afe',
-//		'#304ffe', '#e3f2fd', '#bbdefb', '#90caf9', '#64b5f6',
-//		'#42a5f5', '#2196f3', '#1e88e5', '#1976d2', '#1565c0',
-//		'#0d47a1', '#82b1ff', '#448aff', '#2979ff', '#2962ff',
-//		'#b3e5fc', '#81d4fa', '#4fc3f7', '#29b6f6', '#03a9f4',
-//		'#039be5', '#0288d1', '#0277bd', '#01579b', '#80d8ff',
-//		'#40c4ff', '#00b0ff', '#0091ea', '#e0f7fa', '#b2ebf2',
-//		'#80deea', '#4dd0e1', '#26c6da', '#00bcd4', '#00acc1',
-//		'#0097a7', '#00838f', '#006064', '#84ffff', '#18ffff',
-//		'#00e5ff', '#00b8d4', '#e0f2f1', '#b2dfdb', '#80cbc4',
-//		'#4db6ac', '#26a69a', '#009688', '#00897b', '#00796b',
-//		'#00695c', '#a7ffeb', '#64ffda', '#1de9b6', '#00bfa5',
-//		'#e8f5e9', '#c8e6c9', '#a5d6a7', '#81c784', '#66bb6a',
-//		'#4caf50', '#43a047', '#388e3c', '#2e7d32', '#1b5e20',
-//		'#b9f6ca', '#69f0ae', '#00e676', '#00c853', '#f1f8e9',
-//		'#dcedc8', '#c5e1a5', '#aed581', '#9ccc65', '#8bc34a',
-//		'#7cb342', '#689f38', '#558b2f', '#33691e', '#ccff90',
-//		'#b2ff59', '#76ff03', '#64dd17', '#f9fbe7', '#f0f4c3',
-//		'#e6ee9c', '#dce775', '#d4e157', '#cddc39', '#c0ca33',
-//		'#afb42b', '#9e9d24', '#827717', '#f4ff81', '#eeff41',
-//		'#c6ff00', '#aeea00', '#fffde7', '#fff9c4', '#fff59d',
-//		'#fff176', '#ffee58', '#ffeb3b', '#fdd835', '#fbc02d',
-//		'#f9a825', '#f57f17', '#ffff8d', '#ffff00', '#ffea00',
-//		'#ffd600', '#fff8e1', '#ffecb3', '#ffe082', '#ffd54f',
-//		'#ffca28', '#ffc107', '#ffb300', '#ffa000', '#ff8f00',
-//		'#ff6f00', '#ffe57f', '#ffd740', '#ffc400', '#ffab00',
-//		'#fff3e0', '#ffe0b2', '#ffcc80', '#ffb74d', '#ffa726',
-//		'#ff9800', '#fb8c00', '#f57c00', '#ef6c00', '#e65100',
-//		'#ffd180', '#ffab40', '#ff9100', '#ff6d00', '#fbe9e7',
-//		'#ffccbc', '#ffab91', '#ff8a65', '#ff7043', '#ff5722',
-//		'#f4511e', '#e64a19', '#d84315', '#bf360c', '#ff9e80',
-//		'#ff6e40', '#ff3d00', '#dd2c00', '#d7ccc8', '#bcaaa4',
-//		'#795548', '#d7ccc8', '#bcaaa4', '#8d6e63', '#eceff1',
-//		'#cfd8dc', '#b0bec5', '#90a4ae', '#78909c', '#607d8b',
-//		'#546e7a', '#cfd8dc', '#b0bec5', '#78909c' ];
-//
-//	function randomColor() {
-//		return COLORS[Math.floor(Math.random() * COLORS.length)];
-//	}
-//
-//	function randomSpan() {
-//		var r = Math.random();
-//		if (r < 0.8) {
-//			return 1;
-//		} else if (r < 0.9) {
-//			return 2;
-//		} else {
-//			return 3;
-//		}
-//	}
-//
-//	function openSetting(tile) {
-//		$settings.openConfig(tile.page);
-//	}
-//
-//	// Load settings
-//	$settings.configs()//
-//	.then(function(settings) {
-//		$scope.settingsTiles = [];
-//		for (var i = 0; i < settings.items.length; i++) {
-//			$scope.settingsTiles.push({
-//				color : randomColor(),
-//				colspan : randomSpan(),
-//				rowspan : randomSpan(),
-//				page : settings.items[i]
-//			});
-//		}
-//	});
-//
-//	$scope.openSetting = openSetting;
-//});
 
 'use strict';
 
@@ -5658,7 +5541,7 @@ angular.module('mblowfish-core').run(['$templateCache', function($templateCache)
 
 
   $templateCache.put('views/directives/mb-user-menu.html',
-    "<div md-colors=\"{'background-color': 'primary-400'}\" class=amd-user-menu> <md-menu md-offset=\"0 20\"> <md-button class=amd-user-menu-button ng-click=$mdOpenMenu() aria-label=\"Open menu\"> <img height=32px class=img-circle ng-src={{app.user.current.avatar}}> <span>{{app.user.current.first_name}} {{app.user.current.last_name}}</span> <wb-icon class=material-icons>keyboard_arrow_down</wb-icon> </md-button> <md-menu-content width=3>  <md-menu-item ng-if=menu.items.length ng-repeat=\"item in menu.items | orderBy:['-priority']\"> <md-button ng-click=item.exec($event) translate> <wb-icon ng-if=item.icon>{{item.icon}}</wb-icon> <span ng-if=item.title translate>{{item.title}}</span> </md-button> </md-menu-item> <md-menu-divider ng-if=menu.items.length></md-menu-divider> <md-menu-item> <md-button ng-click=settings() translate>Settings</md-button> </md-menu-item> <md-menu-item> <md-button ng-click=logout() translate>Log out</md-button> </md-menu-item> </md-menu-content> </md-menu> </div>"
+    "<div md-colors=\"{'background-color': 'primary-400'}\" class=amd-user-menu> <md-menu md-offset=\"0 20\"> <md-button class=amd-user-menu-button ng-click=$mdOpenMenu() aria-label=\"Open menu\"> <img height=32px class=img-circle ng-src={{app.user.current.avatar}}> <span>{{app.user.current.first_name}} {{app.user.current.last_name}}</span> <wb-icon class=material-icons>keyboard_arrow_down</wb-icon> </md-button> <md-menu-content width=3>  <md-menu-item ng-if=menu.items.length ng-repeat=\"item in menu.items | orderBy:['-priority']\"> <md-button ng-click=item.exec($event) translate> <wb-icon ng-if=item.icon>{{item.icon}}</wb-icon> <span ng-if=item.title translate=\"\">{{item.title}}</span> </md-button> </md-menu-item> <md-menu-divider ng-if=menu.items.length></md-menu-divider> <md-menu-item> <md-button ng-click=settings() translate>Settings</md-button> </md-menu-item> <md-menu-item> <md-button ng-click=logout() translate>Log out</md-button> </md-menu-item> </md-menu-content> </md-menu> </div>"
   );
 
 
