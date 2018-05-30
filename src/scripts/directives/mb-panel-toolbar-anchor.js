a3f5055cc6ef4d9fee8b8073b0ecfa3a35ee2a42
hadi <hadi@dpq.co.ir>
2018-05-31 00:26:23 +0430

    fix bug in visibility of toolbars

--- src/scripts/directives/mb-panel-toolbar-anchor.js
+++ src/scripts/directives/mb-panel-toolbar-anchor.js
@@ -25,179 +25,175 @@
 angular.module('mblowfish-core')
 
 .directive('mbPanelToolbarAnchor', function($navigator, $usr, $route, $window, $rootScope,
-        $app, $translate, $http, $mdSidenav, $mdBottomSheet, $q, $widget, $controller, $compile) {
+		$app, $translate, $http, $mdSidenav, $mdBottomSheet, $q, $widget, $controller, $compile) {
+
+	/*
+	 * Load page and create an element
+	 */
+	function _loadPage($scope, page, prefix, postfix) {
+		// 1- create scope
+		var childScope = $scope.$new(false, $scope);
+		childScope = Object.assign(childScope, {
+			app : $rootScope.app,
+			_page : page,
+			_visible : function() {
+				if (angular.isFunction(this._page.visible)) {
+					var v = this._page.visible(this);
+					return v;
+				}
+				return true;
+			}
+		});
+
+		// 2- create element
+		return $widget.getTemplateFor(page)
+		.then(function(template) {
+			var element = angular.element(prefix + template + postfix);
+
+			// 3- bind controller
+			var link = $compile(element);
+			if (angular.isDefined(page.controller)) {
+				var locals = {
+						$scope : childScope,
+						$element : element,
+				};
+				var controller = $controller(page.controller, locals);
+				if (page.controllerAs) {
+					childScope[page.controllerAs] = controller;
+				}
+				element.data('$ngControllerController', controller);
+			}
+			;
+			return {
+				element : link(childScope),
+				page : page
+			};
+		});
+	}
+
+	function postLink($scope, $element, $attr) {
+		var _sidenaves = [];
+		var _toolbars = [];
 
 
-
-    /*
-     * Load page and create an element
-     */
-    function _loadPage($scope, page, prefix, postfix) {
-        // 1- create scope
-        var childScope = $scope.$new(false, $scope);
-        childScope = Object.assign(childScope, {
-            app : $rootScope.app,
-            _page : page,
-            _visible : function() {
-                if (angular.isFunction(this._page.visible)) {
-                    var v = this._page.visible(this);
-                    return v;
-                }
-                return true;
-            }
-        });
-
-        // 2- create element
-        return $widget.getTemplateFor(page)
-        .then(function(template) {
-            var element = angular.element(prefix + template + postfix);
-
-            // 3- bind controller
-            var link = $compile(element);
-            if (angular.isDefined(page.controller)) {
-                var locals = {
-                        $scope : childScope,
-                        $element : element,
-                };
-                var controller = $controller(page.controller, locals);
-                if (page.controllerAs) {
-                    childScope[page.controllerAs] = controller;
-                }
-                element.data('$ngControllerController', controller);
-            }
-            ;
-            return {
-                element : link(childScope),
-                page : page
-            };
-        });
-    }
-
-    function postLink($scope, $element, $attr) {
-        var _sidenaves = [];
-        var _toolbars = [];
+		/*
+		 * Remove all sidenaves
+		 */
+		function _removeElements(pages, elements) {
+			var cache = [];
+			for(var i = 0; i < elements.length; i++){
+				var flag = false;
+				for(var j = 0; j < pages.length; j++){
+					if(pages[j].id === elements[i].page.id) {
+						flag = true;
+						break;
+					}
+				}
+				if(flag){
+					elements[i].element.detach();
+					elements[i].cached = true;
+					cache.push(elements[i]);
+				} else {
+					elements[i].element.remove();
+				}
+			}
+			return cache;
+		}
 
 
-        /*
-         * Remove all sidenaves
-         */
-        function _removeElements(pages, elements) {
-            var cache = [];
-            for(var i = 0; i < elements.length; i++){
-                var flag = false;
-                for(var j = 0; j < pages.length; j++){
-                    if(pages[j].id === elements[i].page.id) {
-                        flag = true;
-                        break;
-                    }
-                }
-                if(flag){
-                    elements[i].element.detach();
-                    elements[i].cached = true;
-                    cache.push(elements[i]);
-                } else {
-                    elements[i].element.remove();
-                }
-            }
-            return cache;
-        }
+		function _getToolbarElement(page){
+			for(var i = 0; i < _toolbars.length; i++){
+				if(_toolbars[i].page.id == page.id){
+					return $q.when(_toolbars[i]);
+				}
+			}
+
+			var prefix = page.raw ? '' : '<md-toolbar ng-if="_visible()" md-theme="{{app.setting.theme || app.config.theme || \'default\'}}" md-theme-watch layout="column" layout-gt-xs="row" layout-align="space-between stretch">';
+			var postfix = page.raw ? '' : '</md-toolbar>';
+			return _loadPage($scope, page, prefix, postfix)
+			.then(function(pageElement) {
+				_toolbars.push(pageElement);
+			});
+		}
+
+		/*
+		 * Reload toolbars
+		 */
+		function _reloadToolbars(toolbars) {
+			_toolbars = _removeElements(toolbars, _toolbars);
+			var jobs = [];
+			for (var i = 0; i < toolbars.length; i++) {
+				jobs.push(_getToolbarElement(toolbars[i]));
+			}
+			$q.all(jobs) //
+			.then(function() {
+				// Get Anchor
+				var _anchor = $element;
+				// maso, 2018: sort
+				_toolbars.sort(function(a, b){
+					return (a.page.priority || 10) > (b.page.priority || 10);
+				});
+				for (var i = 0; i < _toolbars.length; i++) {
+					var ep = _toolbars[i];
+					if(ep.chached){
+						continue;
+					}
+					_anchor.prepend(ep.element);
+				}
+			});
+		}
+
+		/*
+		 * Reload UI
+		 * 
+		 * - sidenav
+		 * - toolbar
+		 */
+		function _reloadUi(){
+			if(!$route.current){
+				return;
+			}
+			// Toolbars
+			var tids = $route.current.toolbars || $app.defaultToolbars();
+			if(angular.isArray(tids)){
+				var ts = [];
+				var jobs = [];
+				angular.forEach(tids, function(item){
+					jobs.push($app.toolbar(item)
+							.then(function(toolbar){
+								ts.push(toolbar);
+							}));
+				});
+				$q.all(jobs)
+				.then(function(){
+					_reloadToolbars(ts);
+				});
+			}
+		}
+
+		function _isVisible(item){
+			if (angular.isFunction(item.visible)) {
+				var v = item.visible(this);
+				return v;
+			}
+			if(angular.isDefined(item.visible)){
+				// item.visible is defined but is not a function
+				return item.visible;
+			}
+			return true;
+		}
+
+		$scope.$watch(function(){
+			return $route.current;
+		},_reloadUi);
+//		_reloadUi();
+	}
 
 
-        function _getToolbarElement(page){
-            for(var i = 0; i < _toolbars.length; i++){
-                if(_toolbars[i].page.id == page.id){
-                    return $q.when(_toolbars[i]);
-                }
-            }
-
-            var prefix = page.raw ? '' : '<md-toolbar md-theme="{{app.setting.theme || app.config.theme || \'default\'}}" md-theme-watch layout="column" layout-gt-xs="row" layout-align="space-between stretch">';
-            var postfix = page.raw ? '' : '</md-toolbar>';
-            return _loadPage($scope, page, prefix, postfix)
-            .then(function(pageElement) {
-                _toolbars.push(pageElement);
-            });
-        }
-
-        /*
-         * Reload toolbars
-         */
-        function _reloadToolbars(toolbars) {
-            _toolbars = _removeElements(toolbars, _toolbars);
-            var jobs = [];
-            for (var i = 0; i < toolbars.length; i++) {
-                jobs.push(_getToolbarElement(toolbars[i]));
-            }
-            $q.all(jobs) //
-            .then(function() {
-                // Get Anchor
-                var _anchor = $element;
-                // maso, 2018: sort
-                _toolbars.sort(function(a, b){
-                    return (a.page.priority || 10) > (b.page.priority || 10);
-                });
-                for (var i = 0; i < _toolbars.length; i++) {
-                    var ep = _toolbars[i];
-                    if(ep.chached){
-                        continue;
-                    }
-                    _anchor.prepend(ep.element);
-                }
-            });
-        }
-
-        /*
-         * Reload UI
-         * 
-         * - sidenav
-         * - toolbar
-         */
-        function _reloadUi(){
-            if(!$route.current){
-                return;
-            }
-            // Toolbars
-            var tids = $route.current.toolbars || $app.defaultToolbars();
-            if(angular.isArray(tids)){
-                var ts = [];
-                var jobs = [];
-                angular.forEach(tids, function(item){
-                    jobs.push($app.toolbar(item)
-                            .then(function(toolbar){
-                            	if(_isVisible(toolbar)){                            		
-                            		ts.push(toolbar);
-                            	}
-                            }));
-                });
-                $q.all(jobs)
-                .then(function(){
-                    _reloadToolbars(ts);
-                });
-            }
-        }
-        
-        function _isVisible(item){
-            if (angular.isFunction(item.visible)) {
-                var v = item.visible(this);
-                return v;
-            }
-            if(angular.isDefined(item.visible)){
-            	// item.visible is defined but is not a function
-            	return item.visible;
-            }
-            return true;
-        }
-
-        $scope.$watch(function(){
-            return $route.current;
-        },_reloadUi);
-//        _reloadUi();
-    }
-
-
-    return {
-        restrict : 'A',
-//        replace : true,
-//        templateUrl : 'views/directives/mb-panel.html',
-        link : postLink
-    };
+	return {
+		restrict : 'A',
+//		replace : true,
+//		templateUrl : 'views/directives/mb-panel.html',
+		link : postLink
+	};
 });
