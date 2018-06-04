4fdc3b96000e696a2bbdb00a7abaae25c88dc11f
mostafabarmshory <mostafa.barmshory@dpq.co.ir>
2018-06-05 04:12:19 +0430

    Bug fixed: load help id

--- dist/mblowfish-core.js
+++ dist/mblowfish-core.js
@@ -763,12 +763,13 @@ angular.module('mblowfish-core')
  */
 .controller('MbHelpCtrl', function($scope, $rootScope, $route, $http, $translate, $mdUtil, $mdSidenav, $help) {
 	$rootScope.showHelp = false;
+	var lastItem = 'not-found';
+	var lastLoaded;
 
 
 	function _getHelpId(item) {
 		if(!item){
-			// TODO: maso, 2018: what if current item is null
-			return;
+			return lastItem;
 		}
 		var id = item.helpId;
 
@@ -779,6 +780,7 @@ angular.module('mblowfish-core')
 		if (!angular.isDefined(id)) {
 			id = 'not-found';
 		}
+		lastItem = id;
 		return id;
 	}
 
@@ -794,17 +796,20 @@ angular.module('mblowfish-core')
 			// TODO: maso, 2018: cancle old loading
 		}
 		var myId = _getHelpId(item);
+		if(!$scope.showHelp || myId === lastLoaded) {
+			return;
+		}
 		var lang = $translate.use() === 'fa' ? 'fa' : 'en';
-
 		// load content
-		return $scope.helpLoading = $http.get('resources/helps/' + myId + '-' + lang + '.json') //
+		$scope.helpLoading = $http.get('resources/helps/' + myId + '-' + lang + '.json') //
 		.then(function(res) {
 			$scope.helpContent = res.data;
-			$scope.helpLoaded = true;
+			lastLoaded = myId;
 		})//
 		.finally(function(){
 			$scope.helpLoading = false;
 		});
+		return $scope.helpLoading;
 	}
 
 	$scope.closeHelp = function(){
@@ -812,33 +817,36 @@ angular.module('mblowfish-core')
 //		$mdSidenav('help').close();
 	}
 
-	function buildToggler() {
-		var debounceFn =  $mdUtil.debounce(function(){
-			$mdSidenav('help').toggle();
-		},300);
-		return debounceFn;
-	}
+//	function buildToggler() {
+//		var debounceFn =  $mdUtil.debounce(function(){
+//			$mdSidenav('help').toggle();
+//		},300);
+//		return debounceFn;
+//	}
 
 	/*
 	 * If user want to display help, content will be loaded.
 	 */
 	$scope.$watch('showHelp', function(){
-		if($scope.showHelp && !$scope.helpLoaded){
-			return _loadHelpContent();
-		}
+		return _loadHelpContent();
 	});
+
+	/*
+	 * Watch current state changes
+	 */
+	$scope.$watch(function(){
+		if($route.current){
+			return $route.current.$$route;
+		}
+		return null;
+	}, _loadHelpContent);
 
 	/*
 	 * Watch for current item in help service
 	 */
 	$scope.$watch(function(){
 		return $help.currentItem();
-	}, function(newValue){
-		$scope.helpLoaded = false;
-		if($scope.showHelp){
-			return _loadHelpContent(newValue);
-		}
-	});
+	}, _loadHelpContent);
 });
 // TODO: should be moved to mblowfish-core
 
