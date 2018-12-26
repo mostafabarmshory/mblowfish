7ff21453130fe5a362c1851aa8fab4099f989820
masood <masood@192.168.1.105>
2018-12-26 08:44:25 +0330

    https://trello.com/c/ncpoZuZZ/154-as-bordbar-i-want-to-run-a-shop-as-soon-as-possible
    Related to Fav-icon, two files were brought to the module from angular-material-home

--- dist/mblowfish-core.js
+++ dist/mblowfish-core.js
@@ -5210,6 +5210,89 @@ angular.module('mblowfish-core')
 'use strict';
 
 angular.module('mblowfish-core')
+/**
+ * حالت امنیتی را بررسی می‌کند
+ * 
+ * در صورتی که یک حالت حالتی امن باشد، و کاربر وارد سیستم نشده باشد، حالت ماشین
+ * را به حالت لاگین می‌برد.
+ */
+.run(function($rootScope, $page, $location) {
+
+	/**
+	 * Rests settings of page (title, description, keywords and favicon) to values defined in branding
+	 */
+	function _initBranding() {
+		if($rootScope.app.config){			
+			$page.setTitle($rootScope.app.config.title);
+			$page.setDescription($rootScope.app.config.description);
+			$page.setKeywords($rootScope.app.config.keywords);
+			$page.setFavicon($rootScope.app.config.favicon || $rootScope.app.config.logo);
+		}
+	}
+
+	/**
+	 * If an item of settings of page does not set yet, sets it by value defined in branding
+	 */
+	function _fillUnsetFields() {
+		var config = $rootScope.app.config ? $rootScope.app.config : null;
+		if(!config){
+			return;
+		}
+		$page.setTitle($page.getTitle() || config.title);
+		$page.setDescription($page.getDescription() || config.description);
+		$page.setKeywords($page.getKeywords() || config.keywords);
+		$page.setFavicon(config.favicon || config.logo);
+		$page.setMeta('og:site_name', config.title);
+	}
+	/*
+	 * Listen on change route
+	 */
+	$rootScope.$on('$routeChangeStart', function( /* event */ ) {
+		_initBranding();
+	});
+	$rootScope.$on('$routeChangeSuccess', function( /*event, current*/ ) {
+		var path = $location.absUrl();
+		$page.setMeta('twitter:url', path) //
+		.setMeta('og:url', path);
+	});
+
+	$rootScope.$watch(function(){
+		var conf = $rootScope.app.config;
+		if(!conf){
+			return conf;
+		}
+		return conf.title +'#'+ conf.description +'#'+ conf.keywords +'#'+ conf.logo +'#'+ conf.favicon;
+	}, function() {
+		_fillUnsetFields();
+	});
+
+	$page.setMeta('twitter:card', 'summary');
+	$page.setMeta('og:type', 'object');
+});
+/*
+ * Copyright (c) 2015-2025 Phoinex Scholars Co. http://dpq.co.ir
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+'use strict';
+
+angular.module('mblowfish-core')
 /*
  * Help dialog 
  */
@@ -7326,6 +7409,233 @@ angular.module('mblowfish-core')
 			newPage : newPage
 	};
 	return app;
+});
+
+/*
+ * Copyright (c) 2015-2025 Phoinex Scholars Co. http://dpq.co.ir
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+'use strict';
+
+// TODO: hadi: move it to new module angular-material-home-seo
+angular.module('mblowfish-core')
+
+/**
+ * @ngdoc service
+ * @name $page
+ * @description A page management service
+ * 
+ * 
+ * 
+ */
+.service('$page', function($rootScope, $rootElement) {
+
+
+
+	/*
+	 * <!-- OG -->
+	 * <meta property="og:site_name" content="$title">
+	 */
+
+	$rootScope.page = {
+		title: '',
+		description: '',
+		keywords: [],
+		links:[]
+	};
+	var page = $rootScope.page;
+
+	/**
+	 * 
+	 * @param title
+	 * @returns
+	 */
+	function setTitle(title){
+		page.title = title;
+		var head = $rootElement.find('head');
+		var elements = head.find('title');
+		var metaElement;
+		if(elements.length === 0){
+			// title element not found
+			metaElement = angular.element('<title></title>');
+			head.append(metaElement);
+		} else {
+			metaElement = angular.element(elements[0]);
+		}
+		metaElement.text(title);
+		setMeta('twitter:title', title);
+		setMetaOg('og:title', title);
+		return this;
+	}
+
+	/**
+	 * 
+	 * @returns
+	 */
+	function getTitle(){
+		return page.title;
+	}
+
+	/**
+	 * 
+	 * @param description
+	 * @returns
+	 */
+	function setDescription(description){
+		page.description = description;
+		setMeta('description', description);
+		setMeta('twitter:description', description);
+		setMetaOg('og:description', description);
+		return this;
+	}
+
+	/**
+	 * 
+	 * @returns
+	 */
+	function getDescription(){
+//		return getMeta('description');
+		return page.description;
+	}
+
+	/**
+	 * 
+	 * @param keywords
+	 * @returns
+	 */
+	function setKeywords(keywords){
+		page.keywords = keywords;
+		setMeta('keywords', keywords);
+		return this;
+	}
+
+	/**
+	 * 
+	 * @returns
+	 */
+	function getKeywords(){
+//		return getMeta('keywords');
+		return page.keywords;
+	}
+	
+	function setFavicon(favicon){
+		updateLink('favicon-link', {
+			href: favicon,
+			rel: 'icon'
+		});
+		setMeta('twitter:image', favicon);
+		setMetaOg('og:image', favicon);
+		return this;
+	}
+
+	function updateLink(key, data){
+		var searchkey = key.replace(new RegExp(':', 'g'), '\\:');
+		var head = $rootElement.find('head');
+		var elements = head.find('link[key='+searchkey+']');
+		var metaElement;
+		if(elements.length === 0){
+			// title element not found
+			metaElement = angular.element('<link key=\''+key+'\' />');
+			head.append(metaElement);
+		} else {
+			metaElement = angular.element(elements[0]);
+		}
+		for (var property in data) {
+			metaElement.attr(property, data[property]);
+		}
+		return this;
+	}
+
+	function setMeta(key, value){
+		var searchkey = key.replace(new RegExp(':', 'g'), '\\:');
+		var head = $rootElement.find('head');
+		var elements = head.find('meta[name='+searchkey+']');
+		var metaElement;
+		if(elements.length === 0){
+			// title element not found
+			metaElement = angular.element('<meta name=\''+key+'\' content=\'\' />');
+			head.append(metaElement);
+		} else {
+			metaElement = angular.element(elements[0]);
+		}
+		metaElement.attr('content', value);
+		return this;
+	}
+	
+	/**
+	 * Adds or set an OG meta tag to document.
+	 * Note: OG meta tag is differ than usual meta tags. Attributes of an OG meta tag are: property and content
+	 * while attributes of an usual meta tag are: name and content. 
+	 */
+	function setMetaOg(key, value){
+		var searchkey = key.replace(new RegExp(':', 'g'), '\\:');
+		var head = $rootElement.find('head');
+		var elements = head.find('meta[name='+searchkey+']');
+		var metaElement;
+		if(elements.length === 0){
+			// title element not found
+			metaElement = angular.element('<meta property=\''+key+'\' content=\'\' />');
+			head.append(metaElement);
+		} else {
+			metaElement = angular.element(elements[0]);
+		}
+		metaElement.attr('content', value);
+		return this;
+	}
+
+	/**
+	 * Adds or set an OG meta tag to document.
+	 * Note: OG meta tag is differ than usual meta tags. Attributes of an OG meta tag are: property and content
+	 * while attributes of an usual meta tag are: name and content. 
+	 */
+	function setMetaOg(key, value){
+		var searchkey = key.replace(new RegExp(':', 'g'), '\\:');
+		var head = $rootElement.find('head');
+		var elements = head.find('meta[name='+searchkey+']');
+		var metaElement;
+		if(elements.length === 0){
+			// title element not found
+			metaElement = angular.element('<meta property=\''+key+'\' content=\'\' />');
+			head.append(metaElement);
+		} else {
+			metaElement = angular.element(elements[0]);
+		}
+		metaElement.attr('content', value);
+		return this;
+	}
+	
+	/*
+	 * Service struct
+	 */
+	return {
+		// Init
+		setTitle: setTitle,
+		getTitle: getTitle,
+		setDescription: setDescription,
+		getDescription: getDescription,
+		setKeywords: setKeywords,
+		getKeywords: getKeywords,
+		setFavicon: setFavicon,
+		setMeta: setMeta,
+		setLink: updateLink
+	};
 });
 
 /*
