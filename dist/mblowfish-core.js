fcec15aa70aa35f6f86a6b814820f71df1a20495
masood <masood@192.168.1.105>
2019-01-05 18:13:26 +0330

    transfer currencyFilter from shop module to mblowfish-core

--- dist/mblowfish-core.js
+++ dist/mblowfish-core.js
@@ -4856,6 +4856,79 @@ angular.module('mblowfish-core')
 	    };
 	});
 /*
+ * Copyright (c) 2015-2025 Phoinex Scholars Co. http://dpq.co.ir
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+/* jslint todo: true */
+/* jslint xxx: true */
+/* jshint -W100 */
+'use strict';
+
+angular.module('mblowfish-core')
+
+.filter('currencyFilter', function (numberFilter, translateFilter) {
+
+    return function (price, unit) {
+
+        if (!price) {
+            return translateFilter('free');
+        }
+        if (unit === 'iran-rial' || unit === 'iran-tooman') {
+            return numberFilter(price) + ' '
+                    + translateFilter(unit);
+        } else if (unit === 'bahrain-dinar') {
+            return numberFilter(price) + ' '
+                    + translateFilter('bahrain-dinar');
+        } else if (unit === 'euro') {
+            return numberFilter(price) + ' '
+                    + translateFilter('euro');
+        } else if (unit === 'dollar') {
+            return translateFilter('dollar') + ' '
+                    + numberFilter(price);
+        } else if (unit === 'pound') {
+            return translateFilter('pound') + ' '
+                    + numberFilter(price);
+        } else if (unit === 'iraq-dinar') {
+            return numberFilter(price) + ' '
+                    + translateFilter('iraq-dinar');
+        } else if (unit === 'kuwait-dinar') {
+            return numberFilter(price) + ' '
+                    + translateFilter('kuwait-dinar');
+        } else if (unit === 'oman-rial') {
+            return numberFilter(price) + ' '
+                    + translateFilter('oman-rial');
+        } else if (unit === 'turkish-lira') {
+            return numberFilter(price) + ' '
+                    + translateFilter('turkish-lira');
+        } else if (unit === 'uae-dirham') {
+            return numberFilter(price) + ' '
+                    + translateFilter('uae-dirham');
+        } else {
+            return numberFilter(price) + ' ?';
+        }
+    };
+});
+
+
+
+/*
  * Copyright (c) 2015 Phoenix Scholars Co. (http://dpq.co.ir)
  * 
  * Permission is hereby granted, free of charge, to any person obtaining a copy
