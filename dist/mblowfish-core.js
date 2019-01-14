7818b51877d9cb331fd7424d76facfbfdc03f143
ali.abdollahi.64@gmail.com <ali.abdollahi.64@gmail.com>
2019-01-14 01:53:27 -0800

    https://trello.com/c/lQp5Xlai/227-login-form-does-not-work-in-the-amh-apps-which-have-dependency-to-angular-material-home-dashboard

--- dist/mblowfish-core.js
+++ dist/mblowfish-core.js
@@ -237,6 +237,7 @@ angular.module('mblowfish-core')
     .when('/users/login', {
         templateUrl : 'views/users/mb-login.html',
         controller : 'MbAccountCtrl',
+        controllerAs: 'ctrl',
         sidenavs: [],
         toolbars: []
     })
