ae8e04143c98529155238180e4987a5f2c4fbb3a
Mostafa Barmshory <mostafa.barmshory@gmail.com>
2020-08-13 02:45:12 +0430

    new build of the project

--- dist/mblowfish-core.js
+++ dist/mblowfish-core.js
@@ -9434,10 +9434,32 @@ mblowfish.directive('mbTranslate', function($mbTranslate, $interpolate, $compile
  *
  * @returns {function} Handler function
  */
+mblowfish.factory('$mbTranslateMissingTranslationHandlerStorage', function($mbStorage) {
+	'ngInject';
+	return function(translationId) {
+		if (_.isUndefined($mbStorage['missing-translation'])) {
+			$mbStorage['missing-translation'] = {};
+		}
+		$mbStorage['missing-translation'][translationId] = translationId;
+	};
+});
+
+
+/**
+ * @ngdoc object
+ * @name pascalprecht.translate.$translateMissingTranslationHandlerLog
+ * @requires $log
+ *
+ * @description
+ * Uses angular's `$log` service to give a warning when trying to translate a
+ * translation id which doesn't exist.
+ *
+ * @returns {function} Handler function
+ */
 mblowfish.factory('$mbTranslateMissingTranslationHandlerLog', function($log) {
 	'ngInject';
 	return function(translationId) {
-		$log.warn('Translation for ' + translationId + ' doesn\'t exist');
+		$log.warn('Translation for "' + translationId + '" doesn\'t exist');
 	};
 });
 
