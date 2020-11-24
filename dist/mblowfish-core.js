bb92d453940596ca6879d4ed5f854a1691e048ef
Mostafa Barmshory <mostafa.barmshory@gmail.com>
2020-11-24 04:56:11 +0330

    New build of the project

--- dist/mblowfish-core.js
+++ dist/mblowfish-core.js
@@ -1272,17 +1272,14 @@ to the dashbord by addin action into it.
 var mbApplicationModule = angular
 	.module('mblowfish-core', [ //
 		//	Angular
-		'ngAnimate',
-		'ngAria',
+//		'ngAnimate',
+//		'ngAria',
 		'ngCookies',
-		'ngMaterial',
 		'ngMessages',
 		'ngSanitize',
-		//	AM-WB
-		//		'am-wb-core',
-		//	Others
+		
+		'ngMaterial',
 		'lfNgMdFileInput', // https://github.com/shuyu/angular-material-fileinput
-
 		'ng-appcache',//
 		'angular-material-persian-datepicker',
 	])
